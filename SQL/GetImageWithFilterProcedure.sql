9b9613d3f0c42d2e6bf435de81e1d375073c380e
FrozenDragon60Rus <naum6666@yandex.ru>
2024-04-17 10:11:55 +0300

    SQL Update

--- SQL/GetImageWithFilterProcedure.sql
+++ SQL/GetImageWithFilterProcedure.sql
@@ -17,83 +17,83 @@ CREATE OR ALTER PROC GetImageListWithFilter(
 AS
 BEGIN
 
-	IF OBJECT_ID('tempdb.dbo.#ListTag') IS NOT NULL
+	IF OBJECT_ID('tempdb..#ListTag') IS NOT NULL
 			DROP TABLE dbo.#ListTag;
 		CREATE TABLE dbo.#ListTag(value VARCHAR(50));
 
 	IF @Tag IS NOT NULL
 	BEGIN
 		
-		INSERT INTO dbo.#ListTag
+		INSERT INTO #ListTag
 		SELECT *
 		FROM STRING_SPLIT(@Tag,',');
 
 		DECLARE @TagCount INT;
-		SET @TagCount = (SELECT COUNT(dbo.#ListTag.value) FROM dbo.#ListTag)
+		SET @TagCount = (SELECT COUNT(#ListTag.value) FROM #ListTag)
 	END;
 
-	IF OBJECT_ID('tempdb.dbo.#ListCharacter') IS NOT NULL
+	IF OBJECT_ID('tempdb..#ListCharacter') IS NOT NULL
 			DROP TABLE dbo.#ListCharacter;
 		CREATE TABLE dbo.#ListCharacter(value VARCHAR(50));
 
 	IF @Character IS NOT NULL
 	BEGIN
-		INSERT INTO dbo.#ListCharacter
+		INSERT INTO #ListCharacter
 		SELECT *
 		FROM STRING_SPLIT(@Character,',');
 
 		DECLARE @CharacterCount INT;
-		SET @CharacterCount = (SELECT COUNT(dbo.#ListCharacter.value) FROM dbo.#ListCharacter)
+		SET @CharacterCount = (SELECT COUNT(#ListCharacter.value) FROM #ListCharacter)
 	END;
 
-	IF OBJECT_ID('tempdb.dbo.#ListAuthor') IS NOT NULL
-			DROP TABLE dbo.#ListAuthor;
-		CREATE TABLE dbo.#ListAuthor(value VARCHAR(50));
+	IF OBJECT_ID('tempdb..#ListAuthor') IS NOT NULL
+			DROP TABLE #ListAuthor;
+		CREATE TABLE #ListAuthor(value VARCHAR(50));
 
 	IF @Author IS NOT NULL
 	BEGIN
 
-		INSERT INTO dbo.#ListAuthor
+		INSERT INTO #ListAuthor
 		SELECT *
 		FROM STRING_SPLIT(@Author,',');
 
 		DECLARE @AuthorCount INT;
-		SET @AuthorCount = (SELECT COUNT(dbo.#ListAuthor.value) FROM dbo.#ListAuthor)
+		SET @AuthorCount = (SELECT COUNT(#ListAuthor.value) FROM #ListAuthor)
 	END;
 
 	SELECT [Address]
 	FROM Image
 	LEFT JOIN(
-		SELECT ImageByTag.Image_Id, COUNT(dbo.#ListTag.value) AS Tag_Count
+		SELECT ImageByTag.Image_Id, COUNT(#ListTag.value) AS Tag_Count
 		FROM ImageByTag
 		LEFT JOIN Tag
-		RIGHT JOIN dbo.#ListTag
-		ON Tag.Name = dbo.#ListTag.value
+		RIGHT JOIN #ListTag
+		ON Tag.Name = #ListTag.value
 		ON Tag.Id = ImageByTag.Tag_Id
 		GROUP BY ImageByTag.Image_Id
-		HAVING COUNT(dbo.#ListTag.value) = @TagCount
+		HAVING COUNT(#ListTag.value) = @TagCount
 	) AS JoinTag 
 	ON Image.Id = JoinTag.Image_Id
 	LEFT JOIN(
-		SELECT ImageByCharacter.Image_Id, COUNT(dbo.#ListCharacter.value) AS Character_Count
+		SELECT ImageByCharacter.Image_Id, COUNT(#ListCharacter.value) AS Character_Count
 		FROM ImageByCharacter
 		LEFT JOIN Character
-		RIGHT JOIN dbo.#ListCharacter
-		ON Character.Name = dbo.#ListCharacter.value
+		RIGHT JOIN #ListCharacter
+		ON Character.Name = #ListCharacter.value
 		ON Character.Id = ImageByCharacter.Character_Id
 		GROUP BY ImageByCharacter.Image_Id
-		HAVING COUNT(dbo.#ListCharacter.value) = @CharacterCount
+		HAVING COUNT(#ListCharacter.value) = @CharacterCount
 	) AS JoinCharacter 
 	ON Image.Id = JoinCharacter.Image_Id 
 	LEFT JOIN(
-		SELECT ImageByAuthor.Image_Id, COUNT(dbo.#ListAuthor.value) AS Author_Count
+		SELECT ImageByAuthor.Image_Id, COUNT(#ListAuthor.value) AS Author_Count
 		FROM ImageByAuthor
 		LEFT JOIN Author
-		RIGHT JOIN dbo.#ListAuthor
-		ON Author.Name = dbo.#ListAuthor.value
+		RIGHT JOIN #ListAuthor
+		ON Author.Name = #ListAuthor.value
 		ON Author.Id = ImageByAuthor.Author_Id
 		GROUP BY ImageByAuthor.Image_Id
-		HAVING COUNT(dbo.#ListAuthor.value) = @AuthorCount
+		HAVING COUNT(#ListAuthor.value) = @AuthorCount
 	) AS JoinAuthor 
 	ON Image.Id = JoinAuthor.Image_Id 
 	WHERE Rating BETWEEN @RatingFrom AND @RatingTo
