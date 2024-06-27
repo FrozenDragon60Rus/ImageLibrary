6a8f774f1bbb34208d56a380f00da01f041b4a79
FrozenDragon60Rus <naum6666@yandex.ru>
2024-06-27 15:33:59 +0300

    Test Update

--- ImageDBTest/XML/InfoTest.cs
+++ ImageDBTest/XML/InfoTest.cs
@@ -4,27 +4,39 @@ namespace ImageDBTest.XML
 {
     public class InfoTest : IDisposable
     {
-        public InfoTest() =>
-            Info.DB = string.Empty;
+        private readonly string dbName,
+                                Folder;
+
+        public InfoTest()
+        {
+            dbName = Info.DB.Clone().ToString() ?? string.Empty;
+			Folder = Info.Folder.Clone().ToString() ?? string.Empty;
+        }
         [Fact]
-        public void SetDBTest()
+        public void InfoDBTest()
         {
             Info.DB = @"%appdatalocal%\MyImageLibrary";
 
             Assert.Equal(@"%appdatalocal%\MyImageLibrary", Info.DB);
-        }
-        [Fact]
-        public void GetDBTest()
-        {
 
-        }
-        [Fact]
-        public void GetFolderTest()
-        {
-            Assert.Equal(@"I:\Яндекс диск\Pictures\Test", Info.Folder);
+            Info.DB = dbName;
         }
 
-        public void Dispose() =>
+		[Fact]
+		public void InfoFolderTest()
+		{
+			Info.Folder = @"MyImageLibrary";
+
+			Assert.Equal(@"MyImageLibrary", Info.Folder);
+
+            Info.Folder = Folder;
+		}
+
+        public void Dispose()
+        {
+            Info.DB = dbName;
+            Info.Folder = Folder;
             GC.SuppressFinalize(this);
+        }
     }
 }
